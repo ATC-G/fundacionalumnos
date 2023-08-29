278c12aa2ca80dff9acee31a1418d5b881320ca5
jgrodriguez9 <jgrodriguez.pro@gmail.com>
2023-08-29 00:31:58 -0500

    order sidebar

--- src/components/Layout/Sidebar.js
+++ src/components/Layout/Sidebar.js
@@ -6,125 +6,125 @@ import MetisMenu from "metismenujs";
 import logoWhite from "../../assets/images/ui-white.png";
 import logoSM from "../../assets/images/logo-sm.png";
 
-function Sidebar(){
-    const ref = useRef();
-    const location = useLocation();
-    
-    useEffect(() => {
-        const pathName = location.pathname;
-    
-        const initMenu = () => {
-          new MetisMenu("#side-menu");
-          let matchingMenuItem = null;
-          const ul = document.getElementById("side-menu");
-          const items = ul.getElementsByTagName("a");
-          for (let i = 0; i < items.length; ++i) {
-            if (pathName === items[i].pathname) {
-              matchingMenuItem = items[i];
-              break;
-            }
-          }
-          if (matchingMenuItem) {
-            activateParentDropdown(matchingMenuItem);
-          }
-        };
-        initMenu();
-    }, [location.pathname]);
+function Sidebar() {
+  const ref = useRef();
+  const location = useLocation();
 
-    useEffect(() => {
-        ref.current.recalculate();
-    });
+  useEffect(() => {
+    const pathName = location.pathname;
 
-    function scrollElement(item) {
-        if (item) {
-          const currentPosition = item.offsetTop;
-          if (currentPosition > window.innerHeight) {
-            ref.current.getScrollElement().scrollTop = currentPosition - 300;
-          }
+    const initMenu = () => {
+      new MetisMenu("#side-menu");
+      let matchingMenuItem = null;
+      const ul = document.getElementById("side-menu");
+      const items = ul.getElementsByTagName("a");
+      for (let i = 0; i < items.length; ++i) {
+        if (pathName === items[i].pathname) {
+          matchingMenuItem = items[i];
+          break;
         }
+      }
+      if (matchingMenuItem) {
+        activateParentDropdown(matchingMenuItem);
+      }
+    };
+    initMenu();
+  }, [location.pathname]);
+
+  useEffect(() => {
+    ref.current.recalculate();
+  });
+
+  function scrollElement(item) {
+    if (item) {
+      const currentPosition = item.offsetTop;
+      if (currentPosition > window.innerHeight) {
+        ref.current.getScrollElement().scrollTop = currentPosition - 300;
+      }
+    }
+  }
+
+  function activateParentDropdown(item) {
+    item.classList.add("active");
+    const parent = item.parentElement;
+    const parent2El = parent.childNodes[1];
+    if (parent2El && parent2El.id !== "side-menu") {
+      parent2El.classList.add("mm-show");
     }
 
-    function activateParentDropdown(item) {
-        item.classList.add("active");
-        const parent = item.parentElement;
-        const parent2El = parent.childNodes[1];
-        if (parent2El && parent2El.id !== "side-menu") {
-          parent2El.classList.add("mm-show");
-        }
-    
-        if (parent) {
-          parent.classList.add("mm-active");
-          const parent2 = parent.parentElement;
-    
-          if (parent2) {
-            parent2.classList.add("mm-show"); // ul tag
-    
-            const parent3 = parent2.parentElement; // li tag
-    
-            if (parent3) {
-              parent3.classList.add("mm-active"); // li
-              parent3.childNodes[0].classList.add("mm-active"); //a
-              const parent4 = parent3.parentElement; // ul
-              if (parent4) {
-                parent4.classList.add("mm-show"); // ul
-                const parent5 = parent4.parentElement;
-                if (parent5) {
-                  parent5.classList.add("mm-show"); // li
-                  parent5.childNodes[0].classList.add("mm-active"); // a tag
-                }
-              }
+    if (parent) {
+      parent.classList.add("mm-active");
+      const parent2 = parent.parentElement;
+
+      if (parent2) {
+        parent2.classList.add("mm-show"); // ul tag
+
+        const parent3 = parent2.parentElement; // li tag
+
+        if (parent3) {
+          parent3.classList.add("mm-active"); // li
+          parent3.childNodes[0].classList.add("mm-active"); //a
+          const parent4 = parent3.parentElement; // ul
+          if (parent4) {
+            parent4.classList.add("mm-show"); // ul
+            const parent5 = parent4.parentElement;
+            if (parent5) {
+              parent5.classList.add("mm-show"); // li
+              parent5.childNodes[0].classList.add("mm-active"); // a tag
             }
           }
-          scrollElement(item);
-          return false;
         }
-        scrollElement(item);
-        return false;
+      }
+      scrollElement(item);
+      return false;
     }
+    scrollElement(item);
+    return false;
+  }
 
-    return (
-        <>
-          <div className="vertical-menu">
-            <div className="navbar-brand-box  mt-1">
-              <Link to="/" className="logo logo-light">
-                <span className="logo-sm">
-                  <img src={logoSM} alt="" height="30" className="rounded" />
-                </span>
-                <span className="logo-lg">
-                  <img src={logoWhite} alt="" height="100" className="rounded" />
-                </span>
-              </Link>
+  return (
+    <>
+      <div className="vertical-menu">
+        <div className="navbar-brand-box  mt-1">
+          <Link to="/" className="logo logo-light">
+            <span className="logo-sm">
+              <img src={logoSM} alt="" height="30" className="rounded" />
+            </span>
+            <span className="logo-lg">
+              <img src={logoWhite} alt="" height="100" className="rounded" />
+            </span>
+          </Link>
+        </div>
+        <div data-simplebar className="h-100 mt-4 bar-menu">
+          <SimpleBar className="h-100" ref={ref}>
+            <div id="sidebar-menu">
+              <ul className="metismenu list-unstyled" id="side-menu">
+                <li className="menu-title">Menu </li>
+                <li>
+                  <Link to="/colegiatura" className="">
+                    <i className="bx bx-calendar"></i>
+                    <span>Colegiatura</span>
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/datos-facturacion" className="">
+                    <i className="bx bx-shield-quarter"></i>
+                    <span>Datos de facturación</span>
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/ayuda" className="">
+                    <i className="bx bx-help-circle"></i>
+                    <span>Ayuda</span>
+                  </Link>
+                </li>
+              </ul>
             </div>
-            <div data-simplebar className="h-100 mt-4 bar-menu">
-              <SimpleBar className="h-100" ref={ref}>
-                  <div id="sidebar-menu">
-                    <ul className="metismenu list-unstyled" id="side-menu">
-                        <li className="menu-title">Menu </li>
-                        <li> 
-                          <Link to="/colegiatura" className="">
-                              <i className="bx bx-calendar"></i>
-                              <span>Colegiatura</span>
-                          </Link> 
-                        </li>
-                        <li>
-                          <Link to="/datos-facturacion" className="">
-                              <i className="bx bx-shield-quarter"></i>
-                              <span>Datos de facturación</span>
-                          </Link>              
-                        </li>
-                        <li>
-                          <Link to="/ayuda" className="">
-                              <i className="bx bx-help-circle"></i>
-                              <span>Ayuda</span>
-                          </Link>              
-                        </li>
-                    </ul>
-                  </div>
-              </SimpleBar>
-            </div>
-            <div className="sidebar-background"></div>
-          </div>
-        </>
-      );
+          </SimpleBar>
+        </div>
+        <div className="sidebar-background"></div>
+      </div>
+    </>
+  );
 }
-export default Sidebar
+export default Sidebar;
