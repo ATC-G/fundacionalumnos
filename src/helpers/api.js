c3f8eadbfc928f087f724dafaa46c09e1bfcdedf
jgrodriguez9 <jgrodriguez.pro@gmail.com>
2023-08-30 17:59:13 -0500

    fix content type

--- src/helpers/api.js
+++ src/helpers/api.js
@@ -13,7 +13,7 @@ const axiosApi = axios.create({
 });
 
 axiosApi.defaults.headers.common["Authorization"] = token;
-axiosApi.defaults.headers.common["Content-Type"] = "multipart/form-data";
+//axiosApi.defaults.headers.common["Content-Type"] = "multipart/form-data";
 
 axiosApi.interceptors.response.use(
   (response) => response,
--- src/helpers/payment.js
+++ src/helpers/payment.js
@@ -1,10 +1,7 @@
 import { post } from "./api";
 import * as url from "./url";
 
-const sendPaymentData = (data) => post(url.PAYMENT, data);
+const sendPaymentData = (data) =>
+  post(url.PAYMENT, data, { "Content-Type": "multipart/form-data" });
 
-
-export {
-    sendPaymentData
-};
-
+export { sendPaymentData };
